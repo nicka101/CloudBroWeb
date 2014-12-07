d88053bfd1fb86b07457873bf1cb1dddc3801ba1
Nick Anstee <nicka101@cloudbro.net>
2014-12-07 19:17:44 +0000

    * Move the blog detail page to /blog/post instead of /blog/view_post/

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^view_post/(?P<post_id>\d+)(?:/.*)?$', views.view_post, name='view_post')
+                       url(r'^post/(?P<post_id>\d+)(?:/.*)?$', views.view_post, name='view_post')
                        )
